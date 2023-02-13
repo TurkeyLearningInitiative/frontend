b7aa0b027b3e24930fdbfa1d7651a5c3218cf82b
ABB65 <bayhanbayramoglu@gmail.com>
2023-02-13 14:03:22 +0300

    Headless UI / heroicons added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import Landing from '../components/landing'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,8 +15,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="container">  
-      <h1 className='text-blue-500'>Turkey Learning Initiative</h1>
+      <main className="container mx-auto">  
+      <Landing />
       </main>
     </>
   )
