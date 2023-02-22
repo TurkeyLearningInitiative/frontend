d51ae637a64525b60464c0a38a4894dbd105ee81
ABB65 <bayhanbayramoglu@gmail.com>
2023-02-22 23:19:40 +0300

    Lecture Card Created

--- src/components/RecentLectures.tsx
+++ src/components/RecentLectures.tsx
@@ -14,34 +14,33 @@ export function RecentLectures() {
   return (
     <section className="container mx-auto py-16 px-2">
       <div>
-      <h2 className="text-secondary-900 text-2xl font-semibold mb-6">
-      Son Eklenenler
-      </h2>
+        <h2 className="text-secondary-900 text-2xl font-semibold mb-6">
+          Son Eklenenler
+        </h2>
         <Swiper
           // install Swiper modules
           modules={[Navigation, Pagination, Scrollbar, A11y]}
-
           slidesPerView={1}
           onSwiper={(swiper) => console.log(swiper)}
           onSlideChange={() => console.log('slide change')}
           breakpoints={{
-            "@0.00": {
+            '@0.00': {
               slidesPerView: 2,
               spaceBetween: 10,
             },
-            "@0.75": {
+            '@0.75': {
               slidesPerView: 4,
               spaceBetween: 20,
             },
-            "@1.00": {
+            '@1.00': {
               slidesPerView: 6,
               spaceBetween: 30,
             },
-            "@1.25": {
+            '@1.25': {
               slidesPerView: 6,
               spaceBetween: 40,
             },
-            "@1.50": {
+            '@1.50': {
               slidesPerView: 8,
               spaceBetween: 50,
             },
